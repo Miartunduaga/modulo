05b8be9f5d0eb32cd641bd367b33c700277bd508
Srgaming <67520105+Miartunduaga@users.noreply.github.com>
2023-12-06 21:33:22 -0500

    brayan es un crak

--- serviio-2.3/bin/serviio-console.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if which xdg-open > /dev/null
-then
-  xdg-open "http://localhost:23423/console"
-elif which gnome-open > /dev/null
-then
-  gnome-open "http://localhost:23423/console"
-fi
